5760479413e264c217c89530830ae6d75415bf84
Jaehyeon <jaehyeonp74@gmail.com>
2023-03-30 16:47:39 +0900

    feat: ListHeader 컴포넌트 구현

--- src/components/List/ListHeader.tsx
+++ src/components/List/ListHeader.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 function ListHeader() {
-  return <div>ListHeader</div>
+  return <Header>제주도감</Header>
 }
+
+const Header = styled.header`
+  position: fixed;
+  top: 14px;
+  font-weight: 800;
+  font-size: 32px;
+`
 
 export default ListHeader
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -2,15 +2,30 @@ import React from 'react'
 import Category from 'components/List/Category'
 import ListHeader from 'components/List/ListHeader'
 import NavBar from 'components/common/NavBar'
+import ListCard from 'components/List/ListCard'
+import styled from '@emotion/styled'
+
+const ListCardExample = {
+  drinkImage: 'https://company.lottechilsung.co.kr/common/images/product_view0204_bh2.jpg',
+  drinkName: '동백꽃, 제주',
+  drinkSize: 365,
+  drinkLevel: 16,
+  drinkPrice: 13000,
+  drinkAteCount: 33,
+  drinkIsAte: true,
+}
 
 function List() {
   return (
-    <>
+    <ListLayout>
       <ListHeader />
       <Category />
+      <ListCard drink={ListCardExample} />
       <NavBar />
-    </>
+    </ListLayout>
   )
 }
 
 export default List
+
+const ListLayout = styled.div``
