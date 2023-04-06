bc7a6977a633590a33e1bc7c2f8093edb65f4b59
Ramon K. Shin <gml4009@gmail.com>
2023-04-06 23:32:46 +0900

    api 파일 통합

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,7 @@
 import axios from "services"
+import { drinkType } from "types/drinkType"
+import { apiConnectType, MarkerDataType } from "types/kakaoMapType"
+import { BottomSheetDataType } from "types/layoutControlType"
 
 
 // const BASE_URL = 'https://tamrazu.store'
@@ -37,6 +40,13 @@ export interface DetailData {
   volume: number
   factory: string
 }
+
+// 4.6 소셜 로그인 백엔드 개편 이후로부터
+// 반드시 header에 authorization:{accessToken}이 있어야 합니다.
+// service에 있는 axios는 로그인 시 interceptor를 통해
+// header를 반영하니, 가급적 service 내 axios 사용 바랍니다.
+
+
 export const getMyPageAlcohol = async (memberId: number) => {
   const response = await axios.get(`/api/v1/members/page/${memberId}`)
   return response.data.data
@@ -60,4 +70,38 @@ export const getMyPage = async (memberId: string) => {
 export const postEatingCount = async (memberId: string, alcoholId: number) => {
   const response = await axios.post(`/api/v1/eats`, { memberId, alcoholId })
   return response.data.message === '등록 성공'
+}
+
+// 04.06. 추가
+
+/**
+ * 주류 순위의 TOP 3를 반환한다. 
+ * 
+ * @author Ramon K Shin 
+ * @version 1.0
+ * @returns 음료 순위 TOP 3 반환
+ */
+export const getRanking = async() => {
+  const response = await axios.get<apiConnectType<drinkType[]>>('/api/v1/alcohols/rank')
+  return response.data.data;
+}
+
+/**
+ * 전체 양조장의 목록을 배열로 반환한다.
+ * @param mId 멤버 아이디
+ * @returns 공장 상세정보 배열
+ */
+export const getFactoryList = async(mId:string) => {
+  const response = await axios.get<apiConnectType<MarkerDataType[]>>(`api/v1/factories?memberId=${mId}`);
+  return response.data.data;
+}
+
+/**
+ * factoryId에 해당하는 양조장의 자세한 정보를 반환한다.
+ * @param factoryId 양조장 아이디
+ * @returns 바텀 시트에 쓰일 정보들 (이름, 주소, 술 목록)
+ */
+export const getFactoryDetail = async(factoryId:number) => {
+  const response = await axios.get<apiConnectType<BottomSheetDataType>>(`api/v1/factories/${factoryId}`);
+  return response.data.data;
 }
