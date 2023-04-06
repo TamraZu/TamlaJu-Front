f5804d97f49cc83f1a11bfd4790190c7139c40f7
Ramon K. Shin <gml4009@gmail.com>
2023-04-06 16:37:38 +0900

    로그아웃 공통함수 생성

---
+++ src/components/auth/KakaoLogout.tsx
@@ -0,0 +1,18 @@
+import { InternalAxiosRequestConfig } from "axios";
+import { useEffect } from "react";
+import axios from "services";
+
+const logout = () => {
+    axios.interceptors.request.use(
+        (config: InternalAxiosRequestConfig<any>) => {
+            config.headers.Authorization = null;
+            config.headers["Content-Type"] = null;
+            return config;
+        })
+}
+
+const KakaoLogout = () => {
+    return <div onClick={logout}>로그아웃</div>
+}
+
+export default KakaoLogout;
