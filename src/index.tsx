a633e296eb5885c07f04057c6638e2c065e740dc
Jaehyeon <jaehyeonp74@gmail.com>
2023-04-13 21:14:48 +0900

    feat: Suspense 전역 설정하기

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -7,32 +7,37 @@ import './index.css'
 import App from './App'
 import Layout from 'components/common/Layout'
 import reportWebVitals from './reportWebVitals'
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from 'recoil'
 import ToastList from 'components/common/toast/List'
+import Loading from 'components/common/Loading'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 const queryClient = new QueryClient({
-  defaultOptions:{
-    queries:{
-      refetchOnWindowFocus:false,
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      refetchOnWindowFocus: false,
       // refetchOnMount:false,
       // staleTime:1000 * 300, // 5분 (300초)
     },
   },
 })
+
 root.render(
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <RecoilRoot>
-          <Layout>
-            <ToastList/>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <RecoilRoot>
+        <Layout>
+          <ToastList />
+          <Suspense fallback={<Loading />}>
             <App />
-            
-          </Layout>
-        </RecoilRoot>
-      </BrowserRouter>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+          </Suspense>
+        </Layout>
+      </RecoilRoot>
+    </BrowserRouter>
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
 )
 
 // If you want to start measuring performance in your app, pass a function
