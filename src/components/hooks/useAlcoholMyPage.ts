23aa73069133f96a82e943e0252a9299f08f656e
Jaehyeon <jaehyeonp74@gmail.com>
2023-04-26 14:52:59 +0900

    feat: Skeleton 컴포넌트 구현 및 전역 Suspense 제거

--- src/components/hooks/useAlcoholMyPage.ts
+++ src/components/hooks/useAlcoholMyPage.ts
@@ -4,6 +4,6 @@ import { MyPageAlcoholType } from 'types/DrinkType'
 
 export function useAlcoholMyPage(): MyPageAlcoholType {
   const fallback = {}
-  const { data = fallback } = useQuery(['mypage'], () => getMyPageAlcohol())
+  const { data = fallback } = useQuery(['mypage'], () => getMyPageAlcohol(), { suspense: true })
   return data
 }
