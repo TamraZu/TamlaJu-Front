ab61f8c11bb138f88298f1d8f140369e0ff1b2ef
Jaehyeon <jaehyeonp74@gmail.com>
2023-04-28 14:23:33 +0900

    feat: 메인페이지 스켈레톤 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import { LatLngType } from 'types/KakaoMapType'
-import { useState, createContext } from 'react'
+import { useState, createContext, Suspense } from 'react'
 import { BottomSheetRecoilPropType } from 'types/LayoutControlType'
 import KakaoMap from 'components/kakaoMap/KakaoMap'
 import BottomSheetContainer from 'components/home/BottomSheet'
@@ -9,6 +9,7 @@ import { BrewerlyType } from 'types/DrinkType'
 import RankingContainer from 'components/common/ranking/RankingContainer'
 import ViewMoreButton from 'components/home/ViewMoreButton'
 import HasAuth from 'components/auth/Auth'
+import Loading from 'components/common/Loading'
 
 const defaultPosition: LatLngType = { lat: 33.3764, lng: 126.5377 }
 
@@ -56,16 +57,17 @@ function Main() {
       >
         <Wrapper>
           <Scroll>
-            <Header>
-              제주 전통주로 특별한
-              <br />
-              일상을 보내보세요
-            </Header>
-
-            <KakaoMap center={defaultPosition} zoom={11} />
-            <Header>전통주 순위</Header>
-            <RankingContainer />
-            <ViewMoreButton />
+            <Suspense fallback={<Loading />}>
+              <Header>
+                제주 전통주로 특별한
+                <br />
+                일상을 보내보세요
+              </Header>
+              <KakaoMap center={defaultPosition} zoom={11} />
+              <Header>전통주 순위</Header>
+              <RankingContainer />
+              <ViewMoreButton />
+            </Suspense>
           </Scroll>
         </Wrapper>
         <BottomSheetContainer />
